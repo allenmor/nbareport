ae78ae80c0c7826150ae16eb87e53873ec0eb9d6
Allen Mordukhaev <allenmor93@gmail.com>
2022-08-26 11:24:13 -0400

    search page working

--- src/components/SearchForPlayer.js
+++ src/components/SearchForPlayer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import SearchForPlayerCard from "./SearchForPlayerCard";
 import "./Search.css";
 
 function SearchForPlayer({ players, teams }) {
   const [search, setsSearch] = useState("");
-  const [filteredPlayer, setFilteredPlayer] = useState({});
+  const [playerStats, setPlayerStats] = useState([]);
+  const [nameImg, setNameImg] = useState([])
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -45,43 +47,75 @@ function SearchForPlayer({ players, teams }) {
     let player = updatedStats.filter((el, i) => {
       return el.name.toLowerCase() === search.toLowerCase();
     });
-    setFilteredPlayer(player[0]);
-    setsSearch('')
+    setPlayerStats(Object.entries(player[0].stats).map((e) => ( { [e[0]]: e[1] } )).reverse());
+    setNameImg(player[0]);
+
+    setsSearch("");
+    
   }
+
 
   //GET SEARCH VALUE
   function handleChange(e) {
     setsSearch(e.target.value);
   }
 
+
   return (
     <div className="searchDiv">
-        <section className="webdesigntuts-workshop">
-          <form onSubmit={handleSubmit} action="" method="">
-            <input
-              onChange={handleChange}
-              value={search}
-              type="search"
-              placeholder="Who are you looking for?"
-            />
-            <button>Search</button>
-          </form>
-        </section>
+      <section className="webdesigntuts-workshop">
+        <form onSubmit={handleSubmit} action="" method="">
+          <input
+            onChange={handleChange}
+            value={search}
+            type="search"
+            placeholder="Who are you looking for?"
+          />
+          <button>Search</button>
+        </form>
+      </section>
       <div className="playerImg">
         <div className="card">
           <img
+          className="playerPic"
             src={
-              filteredPlayer.imgURL
-                ? filteredPlayer.imgURL
+              nameImg.imgURL
+                ? nameImg.imgURL
                 : "https://andscape.com/wp-content/uploads/2017/06/nbalogo.jpg?w=1400"
             }
-            alt={filteredPlayer.name}
+            alt={nameImg.name}
           ></img>
           <div className="playerDescription">
-            <h3>{filteredPlayer.name ? filteredPlayer.name : "Name:"}</h3>
-            <p>{filteredPlayer.pos ? filteredPlayer.pos : "Position:"}</p>
+            <h3>{nameImg.name ? nameImg.name : "Name:"}</h3>
+            <p>{nameImg.pos ? nameImg.pos : "Position:"}</p>
           </div>
         </div>
+      </div>
+      <div className="tableDiv">
+        <table>
+          <caption>Player Stats</caption>
+          <thead>
+            <tr>
+              <th scope="col">Year</th>
+              <th scope="col">Team</th>
+              <th scope="col">GP</th>
+              <th scope="col">PTS</th>
+              <th scope="col">REB</th>
+              <th scope="col">AST</th>
+              <th scope="col">FG%</th>
+              <th scope="col">3P%</th>
+              <th scope="col">3PM</th>
+              <th scope="col">STL</th>
+              <th scope="col">BLK</th>
+              <th scope="col">TOV</th>
+            </tr>
+          </thead>
+          <tbody>
+            {playerStats.map((el, i) => {
+              return <SearchForPlayerCard key={i} stats={el}/>
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
---
+++ src/components/SearchForPlayerCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+function SearchForPlayerCard({stats}) {
+    const [stat, setstat] = useState({})
+    useEffect(() => {
+
+        setstat(stats[Object.keys(stats)[0]])
+    },[stats])
+
+    console.log(stat)
+
+  return (
+    <tr>
+    <td data-label="YEAR">{stat.playoffs ? `${stat.season} Playoffs` : stat.season}</td>
+    <td data-label="TEAM">{stat.tid}</td>
+    <td data-label="GP">{stat.gp}</td>
+    <td data-label="PTS">{(stat.pts / stat.gp).toFixed(1)}</td>
+    <td data-label="REB">{((stat.orb + stat.drb) / stat.gp).toFixed(1)}</td>
+    <td data-label="AST">{(stat.ast / stat.gp).toFixed(1)}</td>
+    <td data-label="FG%">{((stat.fg / stat.fga) * 100).toFixed(1)}%</td>
+    <td data-label="3P%">{((stat.tp / stat.tpa) * 100).toFixed(1)}%</td>
+    <td data-label="3PM">{(stat.tp / stat.gp).toFixed(1)}</td>
+    <td data-label="STL">{(stat.stl / stat.gp).toFixed(1)}</td>
+    <td data-label="BLK">{(stat.blk /  stat.gp).toFixed(1)}</td>
+    <td data-label="TOV">{(stat.tov / stat.gp).toFixed(1)}</td>
+  </tr>
+  
+  )
+}
+
+export default SearchForPlayerCard;
