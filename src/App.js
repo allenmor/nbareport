2b51527a18998562d0a1d95f73f25b1732d2cae2
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-14 19:33:50 -0400

    standings added and nav updated

--- src/App.js
+++ src/App.js
@@ -22,10 +22,6 @@ function App() {
       setPlayers(data.data[0].players)
     })
   },[])
-
-
-
-
   return (
     <>
     <NavBar></NavBar>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,18 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="topnav" id="myTopnav">
-      <Link className="active" to="/">Home</Link>
-      <Link to="/stadiums">Stadiums</Link>
-      <Link to="/stats">Stats</Link>
-      <Link to="/searchforplayer">Search</Link>
-      <Link to="/news">News</Link>
-      <p className="allen"><a href="https://www.linkedin.com/in/allenmord/">Allen Mordukhaev</a></p>
+    <>
+    <div className="topNav sticky" id="myTopnav">
+      <div className="justLinks">
+      <Link className="nav-links nbaReport" to="/">NBA Report</Link>
+      <Link className="nav-links" to="/stadiums">Stadiums</Link>
+      <Link className="nav-links" to="/stats">Stats</Link>
+      <Link className="nav-links"to="/searchforplayer">Search</Link>
+      <Link className="nav-links" to="/news">News</Link>
+      </div>
+      <p className="allen"><a className="allenLink" href="https://www.linkedin.com/in/allenmord/">Allen Mordukhaev</a></p>
     </div>
+    </>
   );
 }
 
