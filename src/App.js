a99af2a453c2cdcc657fde2c84139e5dd63e8eb4
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-17 23:40:22 -0400

    fixed google thing

--- src/App.js
+++ src/App.js
@@ -7,32 +7,34 @@ import Stats from "./components/Stats";
 import NavBar from "./components/NavBar";
 import SearchForPlayer from "./components/SearchForPlayer";
 import TopTenContainer from "./components/TopTenContainer";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [teams, setTeams] = useState([])
-  const [players, setPlayers] = useState([])
+  const [teams, setTeams] = useState([]);
+  const [players, setPlayers] = useState([]);
 
   //FETCH GET INFO
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/allenmor/nbareport/main/db.json')
-    .then(res => res.json())
-    .then(data => {
-
-      setTeams(data.data[0].teams)
-      setPlayers(data.data[0].players)
-    })
-  },[])
+    fetch("https://raw.githubusercontent.com/allenmor/nbareport/main/db.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setTeams(data.data[0].teams);
+        setPlayers(data.data[0].players);
+      });
+  }, []);
   return (
     <>
-    <NavBar></NavBar>
-    <Routes>
-      <Route path="/" element={<FrontPage />} />
-      <Route path="/standings" element={<StandingsContainer />} />
-      <Route path="/stats" element={<Stats/>} />
-      <Route path="/searchforplayer" element={<SearchForPlayer teams={teams} players={players}/>} />
-      <Route path="/news" element={<TopTenContainer />} />
-    </Routes>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/standings" element={<StandingsContainer />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route
+          path="/searchforplayer"
+          element={<SearchForPlayer teams={teams} players={players} />}
+        />
+        <Route path="/news" element={<TopTenContainer />} />
+      </Routes>
     </>
   );
 }
