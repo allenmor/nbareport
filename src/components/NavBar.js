071b5cbe6531042e0b1c9d7c61aa5b5e1e2e69ae
Allen Mordukhaev <allenmor93@gmail.com>
2022-10-31 17:09:53 -0400

    logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
     <>
     <div className="topNav" id="myTopnav">
       <div className="justLinks">
-      <Link className="nav-links nbaReport" to="/">NBA Report</Link>
+      <Link className="nav-links nbaReport" to="/"><img src="https://cdn.nba.com/manage/2020/10/NBA20Secondary20Logo-784x462.jpg"/></Link>
       <Link className="nav-links" to="/standings">Standings</Link>
       <Link className="nav-links" to="/stats">Stats</Link>
       <Link className="nav-links"to="/searchforplayer">Search</Link>
