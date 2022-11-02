0318c9aeb1ac489b7be0eb157f16ad5ee5d44ec7
Allen Mordukhaev <allenmor93@gmail.com>
2022-11-02 15:16:47 -0400

    read more button fixed

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -14,9 +14,8 @@ function HomeCard({info}) {
       <h1 className='eachNews-h1'>{info.title}</h1>
       <p className="newsDate">{info.pubDate}</p>
       <img alt="newsImg" className="newsImg" src={info.thumbnail._url} />
-      { <p className="newsDescription">{info.description.replace(/<[^>]*>?/gm, '')}</p>}
+      { <p className="newsDescription">{clicked ? info.description.replace(/<[^>]*>?/gm, '').split('\n')[0] : info.description.replace(/<[^>]*>?/gm, '')}<button onClick={handleReadMoreClick} className="read-more">{clicked  ? "Read More" : "Read Less"}</button></p>}
     </div>
-      {info.description.replace(/<[^>]*>?/gm).length > 1070 || window.innerWidth < 500 && info.description.replace(/<[^>]*>?/gm).length >357 ? <button onClick={handleReadMoreClick} className="read-more">{clicked  ? "Read More" : "Read Less"}</button> : ''}
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./NavBar.css";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+
+  const navigate = useNavigate()
+  function handleLogoClick(){
+    navigate('/')
+  }
   return (
     <>
     <div className="topNav" id="myTopnav">
       <div className="justLinks">
-      <Link className="nav-links nbaReport" to="/"><img src="https://cdn.nba.com/manage/2020/10/NBA20Secondary20Logo-784x462.jpg"/></Link>
+      <img onClick={handleLogoClick} className="nav-links nbaReport" src="https://cdn.nba.com/manage/2020/10/NBA20Secondary20Logo-784x462.jpg"/>
       <Link className="nav-links" to="/standings">Standings</Link>
       <Link className="nav-links" to="/stats">Stats</Link>
       <Link className="nav-links"to="/searchforplayer">Search</Link>
