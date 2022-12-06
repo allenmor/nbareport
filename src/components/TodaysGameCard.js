3fb4bf22cc49aba89a070d5f6056fadcea24b7ce
Allen Mordukhaev <allenmor93@gmail.com>
2022-12-06 18:46:22 -0500

    added date to each game

--- src/components/TodaysGameCard.js
+++ src/components/TodaysGameCard.js
@@ -7,13 +7,20 @@ import './TodaysGame.css'
 function TodaysGameCard({ game, teamClicked }) {
 
 
-
+console.log(game);
 
   function handleAwayClick() {
     teamClicked(true, game.awayTeam.teamTricode, `${game.awayTeam.teamCity} ${game.awayTeam.teamName}`)
 
   }
 
+  function handleHomeClick() {
+    teamClicked(true, game.homeTeam.teamTricode, `${game.homeTeam.teamCity} ${game.homeTeam.teamName}`)
+
+  }
+
+  let gameDate = `${game.gameDateEst[5]}${game.gameDateEst[6]}/${game.gameDateEst[8]}${game.gameDateEst[9]}/${game.gameDateEst[0]}${game.gameDateEst[1]}${game.gameDateEst[2]}${game.gameDateEst[3]}`
+  console.log(gameDate);
 
   return (
     <>
@@ -25,10 +32,15 @@ function TodaysGameCard({ game, teamClicked }) {
             <img alt="logo" className="team-logos-games" src={game.awayTeam.teamLogo} />
               <p className="team-record">{game.awayTeam.wins} - {game.awayTeam.losses}</p>
         </div>
+        <div className="gameInfoContainer">
+        <p className="gameDate">{gameDate}</p>
+        <div>
         <p className="gameScore">{game.awayTeam.score}</p>
         <p className="time-of-game">{game.gameStatusText.replace('ET', '')}</p>
         <p className="gameScore">{game.homeTeam.score}</p>
-        <div className="away-home">
+        </div>
+        </div>
+        <div onClick={handleHomeClick} className="away-home">
           <h4 className="team-names">
             {game.homeTeam.teamCity} {game.homeTeam.teamName}{" "}
           </h4>
