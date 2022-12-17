696a11d9acbe12cdd845475a91ec6a83782420cf
Allen Mordukhaev <allenmor93@gmail.com>
2022-12-17 15:00:29 -0500

    home team and away team player stats in boxscore

--- src/components/BoxScore.js
+++ src/components/BoxScore.js
@@ -5,11 +5,67 @@ import "./BoxScore.css";
 import { useState, useEffect } from "react";
 
 function BoxScore({ away, home, boxScore, homeName, awayName }) {
-  console.log(boxScore);
+
+const [homeClicked, setHomeClicked] = useState(false)
+const [awayClicked, setAwayClicked] = useState(false)
+const [statsClicked, setStatsClicked] = useState(false)
+const [homeTeamStats, setHomeTeamStats] = useState([])
+const [awayTeamStats, setAwayTeamStats] = useState([])
+
+
+useEffect(()=>{
+    let homeTeamArr = []
+    let awayTeamArr = []
+
+    if(boxScore.playerStats) {
+        
+      let arrOfObj = Object.values(boxScore.playerStats);
+      arrOfObj.map((el, i) => {
+              return el.teamAbv === away && awayTeamArr.push(el)
+          })
+  
+      arrOfObj.map((el, i) => {
+          return el.teamAbv === home && homeTeamArr.push(el)
+      })
+      setHomeTeamStats(homeTeamArr)
+      setAwayTeamStats(awayTeamArr)
+      setAwayClicked(true)
+
+
+       setAwayTeamStats(prev => prev.sort(function(a, b) {
+        return a.mins - b.mins;
+      }).reverse())
+       setHomeTeamStats(prev => prev.sort(function(a, b) {
+        return a.mins - b.mins;
+      }).reverse())
+      
+
+  }
+},[boxScore.playerStats])
+
+console.log(awayTeamStats);
+console.log(homeTeamStats);
+  function handleAwayClick(){
+      setAwayClicked(true)
+      setHomeClicked(false)
+      setStatsClicked(false)
+  }
+  function handleHomeClick(){
+      setHomeClicked(true)
+      setAwayClicked(false)
+      setStatsClicked(false)
+  }
+  function handleStatsClick(){
+      setStatsClicked(true)
+      setHomeClicked(false)
+      setAwayClicked(false)
+  }
 
   return (
     <div className="boxscore-div">
-        {boxScore.lineScore && <table className="total-points-table-div">
+        {boxScore.lineScore && 
+        <div>
+        <table className="total-points-table-div">
             <thead>
                 <tr>
                     <th>Team</th>
@@ -38,7 +94,86 @@ function BoxScore({ away, home, boxScore, homeName, awayName }) {
                     <td>{boxScore.lineScore[home].totalPts}</td>
                 </tr>
             </tbody>
-        </table>}
+        </table>
+        <div className="team-away-stats-btn-div">
+                <button onClick={handleAwayClick}>{awayName}</button>
+                <button onClick={handleHomeClick}>{homeName}</button>
+                {/* <button onClick={handleStatsClick}>Stats</button> */}
+            </div>
+        <div className="player-boxscore-div">
+            <table className="team-boxscore-table">
+                <thead>
+                    <tr>
+                        <th>Player</th>
+                        <th>MIN</th>
+                        <th>PTS</th>
+                        <th>REB</th>
+                        <th>AST</th>
+                        <th>STL</th>
+                        <th>BLK</th>
+                        <th>FGM</th>
+                        <th>FGA</th>
+                        <th>FG%</th>
+                        <th>3PM</th>
+                        <th>3PA</th>
+                        <th>3P%</th>
+                        <th>FTM</th>
+                        <th>FTA</th>
+                        <th>FT%</th>
+                        <th>TOV</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {awayClicked && awayTeamStats.map((el, i) => {
+                       return(<tr className="each-player-in-table" key={i}>
+                           <td className="player-name">{el.longName}</td>
+                           <td>{el.mins}</td>
+                           <td>{el.pts}</td>
+                           <td>{el.reb}</td>
+                           <td>{el.ast}</td>
+                           <td>{el.stl}</td>
+                           <td>{el.blk}</td>
+                           <td>{el.fgm}</td>
+                           <td>{el.fga}</td>
+                           <td>{el.fgp}</td>
+                           <td>{el.tptfgm}</td>
+                           <td>{el.tptfga}</td>
+                           <td>{el.tptfgp}</td>
+                           <td>{el.ftm}</td>
+                           <td>{el.fta}</td>
+                           <td>{el.ftp}</td>
+                           <td>{el.TOV}</td>
+                       </tr>)
+                    })}
+                    
+                    {homeClicked && homeTeamStats.map((el, i) => {
+                       return(<tr className="each-player-in-table" key={i}>
+                           <td className="player-name">{el.longName}</td>
+                           <td>{el.mins}</td>
+                           <td>{el.pts}</td>
+                           <td>{el.reb}</td>
+                           <td>{el.ast}</td>
+                           <td>{el.stl}</td>
+                           <td>{el.blk}</td>
+                           <td>{el.fgm}</td>
+                           <td>{el.fga}</td>
+                           <td>{el.fgp}</td>
+                           <td>{el.tptfgm}</td>
+                           <td>{el.tptfga}</td>
+                           <td>{el.tptfgp}</td>
+                           <td>{el.ftm}</td>
+                           <td>{el.fta}</td>
+                           <td>{el.ftp}</td>
+                           <td>{el.TOV}</td>
+                       </tr>)
+                    })}
+                    
+                </tbody>
+            </table>
+        </div>
+        </div>
+        }
+   
     </div>
   );
 }
